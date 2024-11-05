21c2a83a8bc0ba7177c6c50b20c3bff292e33190
IsmaelBarBus <134372383+IsmaelBarBus@users.noreply.github.com>
2024-11-05 13:19:39 +0100

    Update CalcularPrimos.py

--- pr1Ismael/CalcularPrimos.py
+++ pr1Ismael/CalcularPrimos.py
@@ -1,2 +1,3 @@
-for i in range(5):
+leer por pantalla un numero y sacar con 10 hilos todos los numeros primos que haya del 0 hasta el numero que hemos leido por pantalla
+repartiendo uniformemente el numero al numero de hilos.
   
