d06ec23b43b07f923f30bd87b5700c8b2203d0e9
IsmaelBarBus <134372383+IsmaelBarBus@users.noreply.github.com>
2024-11-19 13:00:38 +0100

    Update ejercicioClase.py

--- ejercicioClase.py
+++ ejercicioClase.py
@@ -1,36 +1,34 @@
 import threading
 
 def preparacion(cond):
-    for _ in range(5):  # Reduce el número de iteraciones para hacerlo más fácil de seguir
+    for _ in range(5): 
       cont = 0
         with cond:
           cont = cont + 1
-            cond.wait()  # Espera a que el otro hilo notifique
-            print("preparacion")
-            cond.notify()  # Notifica al otro hilo para que continúe
+            cond.wait()
+            print("preparacion" + cont + "completado")
+            cond.notify()
 
 def procesamiento(cond):
     for _ in range(5):
       cont = 0
         with cond:
           cont = cont + 1
-            cond.wait()  # Espera a que el otro hilo notifique
-            print("procesamiento")
-            cond.notify()  # Notifica al otro hilo para que continúe
+            cond.wait()  
+            print("procesamiento" + cont + "completado")
+            cond.notify()  
 
 def empaque(cond):
     for _ in range(5):
       cont = 0
         with cond:
           cont = cont + 1
-            cond.wait()  # Espera a que el otro hilo notifique
-            print("empaque")
-            cond.notify()  # Notifica al otro hilo para que continúe
+            cond.wait()
+            print("empaque" + cont + "completado")
+            cond.notify()
 
-# Crear una instancia de Condition para coordinar los hilos
 cond = threading.Condition()
 
-# Crear e iniciar los hilos
 t1 = threading.Thread(target=preparacion, args=(cond,))
 t2 = threading.Thread(target=procesamiento, args=(cond,))
 t3 = threading.Thread(target=empaque, args=(cond,))
@@ -38,11 +36,9 @@ t1.start()
 t2.start()
 t3.start()
 
-# Iniciar la secuencia con una primera notificación
 with cond:
-    cond.notify()  # Notifica al primer hilo para que empiece
+    cond.notify() 
 
-# Esperar a que ambos hilos terminen
 t1.join()
 t2.join()
-t3.join
+t3.join()
